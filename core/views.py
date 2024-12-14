67fd379bbb02b5482041590d497b100ffd2c404e
Carlos Eduardo Rodrigues dos Santos <cers.rodrigues@gmail.com>
2024-12-13 23:29:27 -0300

    Parte 1 do formulário-forms

--- core/views.py
+++ core/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .forms import contatoForm
+from django.contrib import messages
 
 
 def index(request):
@@ -6,7 +8,29 @@ def index(request):
 
 
 def contato(request):
-    return render(request, 'contato.html')
+    form = contatoForm(request.POST or None)
+
+    if str(request.method) == 'POST':
+       if form.is_valid():
+          nome = form.cleaned_data['nome']
+          email = form.cleaned_data['email']
+          assunto = form.cleaned_data['assunto']
+          mensagem = form.cleaned_data['mensagem']
+
+          print('Mensagem Enviada')
+          print(f'Nome: {nome}')
+          print(f'Nome: {email}')
+          print(f'Nome: {assunto}')
+          print(f'Nome: {mensagem}')
+
+          messages.success(request, 'E-mail enviado com sucesso')
+          form = contatoForm()      
+       else:
+          messages.error(request, 'Erro ao enviar e-mail')
+    context = {
+       'form': form
+    }
+    return render(request, 'contato.html', context)
 
 
 def produto(request):
