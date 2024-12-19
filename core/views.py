7fe76ad6c4876e18afd94025db0e290260f11b0a
Carlos Eduardo Rodrigues dos Santos <cers.rodrigues@gmail.com>
2024-12-18 22:36:52 -0300

    trabalhando com formulários parte 2- modelform

--- core/views.py
+++ core/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .forms import contatoForm
+from .forms import contatoForm, ProdutoModelForm
 from django.contrib import messages
 
 
@@ -25,4 +25,25 @@ def contato(request):
 
 
 def produto(request):
-   return render(request, 'produto.html')
+   if str(request.method) == 'POST':
+      form = ProdutoModelForm(request.POST, request.FILES)
+      if form.is_valid():
+         prod = form.save(commit=False)
+
+         print(f'Nome: {prod.nome}')
+         print(f'Preço: {prod.preco}')
+         print(f'Estoque: {prod.estoque}')
+         print(f'Imagem: {prod.imagem}')
+
+         messages.success(request, 'Poduto salvo com sucesso.')
+         form = ProdutoModelForm()
+      else:
+         messages.error(request, 'Erro ao salvar produto')
+   else:
+      form = ProdutoModelForm()
+
+   context = {
+      'form': form
+   }
+
+   return render(request, 'produto.html', context)
